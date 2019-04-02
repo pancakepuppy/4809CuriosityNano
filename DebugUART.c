0a680eb87f19d841c691c666dc7c2c1925a960ae
pancakepuppy <48567546+pancakepuppy@users.noreply.github.com>
2019-04-02 13:06:31 -0700

    Add CDC Debug Output Library

---
+++ DebugUART.c
@@ -0,0 +1,34 @@
+/*
+ * DebugUART.c
+ *
+ * Created: 4/2/2019 7:29:57 AM
+ *  Author: labrat
+ */ 
+#define F_CPU 3333333UL // Set F_CPU
+#define DEBUG_BAUD 19200UL // Set DEBUG_BAUD rate
+#include "DebugUART.h"
+#include <avr/io.h>
+
+void DebugUART_Init()
+{
+	USART3_BAUD = DEBUG_BAUDREG;
+	USART3_DBGCTRL = USART_DBGRUN_bm; // Enable run during debug
+	PORTB_DIRSET = PIN0_bm; // Set TxD as output
+	USART3_CTRLB = USART_TXEN_bm; // Enable transmitter
+}
+
+void DebugUART_TxByte(uint8_t tx_byte)
+{
+	while(!(USART3_STATUS & USART_DREIF_bm)); // Wait for Data Register Empty flag
+	USART3_TXDATAL = tx_byte; // Load byte into Transmit Data Register
+	while(!(USART3_STATUS & USART_TXCIF_bm)); // Wait for Transmit Complete flag
+	USART3_STATUS |= USART_TXCIF_bm; // Clear Transmit Complete flag
+}
+
+void DebugUART_TxString(char *string)
+{
+	for(int i = 0; *(string + i) != '\0'; i++) // Loop through the string until hitting NULL character
+	{
+		DebugUART_TxByte((char)*(string + i)); // Print a byte
+	}	
+}
---
+++ DebugUART.h
@@ -0,0 +1,32 @@
+/*
+ * DebugUART.h
+ *
+ * Helper functions for ATMEGA4809 Curiosity Nano CDC Serial Port
+ * Serial port is USART3 on PB0 (TxD) and PB1 (RxD)
+ * Created: 4/2/2019 7:29:45 AM
+ *  Author: labrat
+ */ 
+#include <avr/io.h>
+
+#ifndef DEBUGUART_H_
+#define DEBUGUART_H_
+
+// Defines
+#ifndef F_CPU
+#error "F_CPU required to be defined"
+#define F_CPU 3333333UL
+#endif
+
+#ifndef DEBUG_BAUD
+#error "DEBUG_BAUD required to be defined"
+#define DEBUG_BAUD 19200UL
+#endif
+
+#define DEBUG_BAUDREG (((F_CPU) * 4UL) / (DEBUG_BAUD))
+
+// Function Prototypes
+void DebugUART_Init();
+void DebugUART_TxByte(uint8_t);
+void DebugUART_TxString(char*);
+
+#endif /* DEBUGUART_H_ */
